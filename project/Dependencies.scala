1ee4455c5a1827de6b67daf7d49eeb74c3769a86
sumito3478 <sumito3478@gmail.com>
2013-12-21 17:14:39 +0900

    Add jackson modules to library dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,6 +8,21 @@ object Dependencies {
 
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.5"
 
+  object jackson {
+    object constants {
+      val version = "2.3.0"
+      val name = "jackson"
+      object group {
+        val prefix = s"com.fasterxml.$name"
+        val core = s"$prefix.core"
+        val module = s"$prefix.module"
+      }
+      val module = s"$name-module"
+    }
+    import constants._
+    val Seq(core, databind) = Seq("core", "databind").map(a => group.core % s"$name-$a" % version)
+    val Seq(afterburner) = Seq("afterburner").map(a => group.module % s"$module-$a" % version)
+  }
   object libraries {
     object constants {
       val test = "test"
@@ -17,6 +32,6 @@ object Dependencies {
     val common = Seq(slf4j, scalatest % test, logback % test)
     val macros = common ++ Seq()
     val core = common ++ Seq()
-    val json = common ++ Seq()
+    val json = common ++ Seq(jackson.core, jackson.databind, jackson.afterburner)
   }
 }
