900420f1c6a6e515f23dd88ff07145c66ae86921
sumito3478 <sumito3478@gmail.com>
2013-12-21 17:13:00 +0900

    Add slf4j and logback to library dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,13 +2,17 @@ import sbt._
 import Keys._
 
 object Dependencies {
+  val logback = "ch.qos.logback" % "logback-classic" % "1.0.13"
+
+  val slf4j = "org.slf4j" % "slf4j-api" % "1.7.5"
+
   object libraries {
     object constants {
       val test = "test"
     }
     import constants._
     private[this] def d = Dependencies
-    val common = Seq()
+    val common = Seq(slf4j, logback % test)
     val macros = common ++ Seq()
     val core = common ++ Seq()
     val json = common ++ Seq()
