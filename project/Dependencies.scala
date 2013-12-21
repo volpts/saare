f975e74dc160108376f50b5dd2648a0bcc58d7b1
sumito3478 <sumito3478@gmail.com>
2013-12-21 17:13:13 +0900

    Add scalatest to library dependencies

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,6 +4,8 @@ import Keys._
 object Dependencies {
   val logback = "ch.qos.logback" % "logback-classic" % "1.0.13"
 
+  val scalatest = "org.scalatest" %% "scalatest" % "2.0"
+
   val slf4j = "org.slf4j" % "slf4j-api" % "1.7.5"
 
   object libraries {
@@ -12,7 +14,7 @@ object Dependencies {
     }
     import constants._
     private[this] def d = Dependencies
-    val common = Seq(slf4j, logback % test)
+    val common = Seq(slf4j, scalatest % test, logback % test)
     val macros = common ++ Seq()
     val core = common ++ Seq()
     val json = common ++ Seq()
