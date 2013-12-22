837525c3a159cf1c40c4f3247e51a027f87210a3
sumito3478 <sumito3478@gmail.com>
2013-12-23 08:09:34 +0900

    License this software under the Apache License 2.0

--- project/Build.scala
+++ project/Build.scala
@@ -1,3 +1,17 @@
+/*Copyright 2013 sumito3478 <sumito3478@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+ */
 import sbt._
 import Keys._
 
@@ -18,6 +32,13 @@ object Build extends Build {
     resolvers += Resolver.sonatypeRepo("releases"),
     libraryDependencies <+= scalaVersion("org.scala-lang" % "scala-reflect" % _),
     addCompilerPlugin("org.scalamacros" % "paradise" % "2.0.0-M1" cross CrossVersion.full),
+    pomExtra := (
+      <licenses>
+        <name>The Apache Software License, Version 2.0</name>
+        <url>http://www.apache.org/licenses/LICENSE-2.0.txt</url>
+        <distribution>repo</distribution>
+      </licenses>
+      ),
     scalacOptions ++= Seq(
       "-encoding", "utf-8",
       "-target:jvm-1.7",
